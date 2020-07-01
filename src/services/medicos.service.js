180f603d09faed7d00ddf505f0e5e17c046cf9b8
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2020-07-01 12:32:10 -0400

    change url

--- src/services/medicos.service.js
+++ src/services/medicos.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://isw-springboot-example-ecreyes.herokuapp.com/api"
+const API_URL = "https://spring-post-new-isw.herokuapp.com/api"
 
 const getMedicos = () => {
   let url = `${API_URL}/medicos`;
